395cb488e5f32a6f27ddf40498cf732c189119f9
AzukiCoconut <136940320+AzukiCoconut@users.noreply.github.com>
2023-10-21 19:45:48 -0300

    Changes to the application through testing

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,7 @@ const {Tag, Product, ProductTag} = require('../../models');
 
 router.get('/', async (req, res) => {
     try {
-        const tagData = await Tag.findAll({
-            include: [{model: Product}],
-        });
+        const tagData = await Tag.findAll();
         res.status(200).json(tagData);
     } catch (err) {
         res.status(500).json(err);
@@ -14,9 +12,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        const tagData = await Tag.findByPk(req.params.id, {
-            include: [{ model: Product }],
-        });
+        const tagData = await Tag.findByPk(req.params.id);
 
         if (!tagData) {
             res.status(404).json({message: 'No tags found with that id!'});
